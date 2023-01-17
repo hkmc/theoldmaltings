c6c463ff84825fb2814fee5825d50eee40a4325b
Huw <huwmcleod@gmail.com>
2023-01-17 16:44:57 +0000

    form

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,7 +1,6 @@
-'use strict';
+"use strict";
 
 $(document).ready(function () {
-
   //$('#navigation').localScroll();
   //$('#navigation').collapse('hide');
 
@@ -14,89 +13,93 @@ $(document).ready(function () {
     options: { scrollwheel: false }
   };
 
-  var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
   var marker = new google.maps.Marker({
     position: latLng,
     map: map,
-    title: 'The Old Maltings'
+    title: "The Old Maltings"
   });
 
-  $('.help-inline').hide();
+  $(".help-inline").hide();
 
-  $('.toggleTrigger').click(function () {
+  $(".toggleTrigger").click(function () {
     //$('.toggleTarget').toggle('slow');
-    if ($('.toggleTarget').is(':hidden')) {
-      $('.toggleTarget').slideDown('slow', 'swing');
-      $(this).html('Hide Terms and Conditions');
+    if ($(".toggleTarget").is(":hidden")) {
+      $(".toggleTarget").slideDown("slow", "swing");
+      $(this).html("Hide Terms and Conditions");
     } else {
-      $('.toggleTarget').hide();
-      $(this).html('Show Terms and Conditions');
+      $(".toggleTarget").hide();
+      $(this).html("Show Terms and Conditions");
     }
 
     return false;
   });
 
-  $('.nav li').click(function () {
-
-    $(this).addClass('active').siblings().removeClass('active');
-    $('#navigation').collapse('hide');
+  $(".nav li").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+    $("#navigation").collapse("hide");
   });
 
-  $('.banner').hover(function () {
-    $(this).find('.banner-caption').slideToggle('fast');
+  $(".banner").hover(function () {
+    $(this).find(".banner-caption").slideToggle("fast");
   });
 
   var hasError = false;
   var showError = function showError(inputSelector) {
     $(inputSelector).siblings().first().show();
-    $(inputSelector).closest('.control-group').addClass('error');
+    $(inputSelector).closest(".control-group").addClass("error");
   };
 
-  $('#enquiry').click(function () {
-    var btn = $(this),
-        hasError = false,
-        _name = $('#inputName').val(),
-        _email = $('#inputEmail').val(),
-        _msg = $('#inputMessage').val();
+  var handleSubmit = function handleSubmit(event) {
+    event.preventDefault();
 
-    $('.control-group').removeClass('error');
-    $('.help-inline').hide();
+    var btn = $("#enquiry"),
+        hasError = false,
+        _name = $("#inputName").val(),
+        _email = $("#inputEmail").val(),
+        _msg = $("#inputMessage").val();
+
+    $(".control-group").removeClass("error");
+    $(".help-inline").hide();
 
     if (_name.length === 0) {
-      showError('#inputName');
+      showError("#inputName");
       hasError = true;
     }
     if (_email.length === 0) {
-      showError('#inputEmail');
+      showError("#inputEmail");
       hasError = true;
     }
     if (_msg.length === 0) {
-      showError('#inputMessage');
+      showError("#inputMessage");
       hasError = true;
     }
     if (hasError) {
       return;
     }
-    btn.button('loading');
+    btn.button("loading");
 
-    $.ajax({
-      url: 'https://formspree.io/theoldmaltingsthornham@gmail.com',
-      method: 'POST',
-      data: { message: _msg, email: _email, name: _name },
-      dataType: 'json',
-      success: function success(data) {
-        btn.siblings().first().show();
-        btn.closest('.control-group').addClass('success');
-        btn.button('reset');
-      },
-      error: function error() {
-        btn.siblings().first().html('Sorry there was an error submitting the form. Please email theoldmaltingsthornham@gmail.com.');
-        btn.siblings().first().show();
-        btn.closest('.control-group').addClass('error');
-        btn.button('reset');
-      }
+    var myForm = event.target;
+    var formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString()
+    }).then(function (res) {
+      btn.siblings().first().show();
+      btn.closest(".control-group").addClass("success");
+      btn.button("reset");
+    }).catch(function (error) {
+      console.log(error);
+      btn.siblings().first().html("Sorry there was an error submitting the form. Please email theoldmaltingsthornham@gmail.com.");
+      btn.siblings().first().show();
+      btn.closest(".control-group").addClass("error");
+      btn.button("reset");
     });
-  });
+  };
+
+  document.querySelector("form").addEventListener("submit", handleSubmit);
 });
 //# sourceMappingURL=main.js.map
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,105 +1,112 @@
-
-
-$(document).ready(function(){
-
+$(document).ready(function () {
   //$('#navigation').localScroll();
   //$('#navigation').collapse('hide');
 
-  var latLng = new google.maps.LatLng(52.959941,0.588637);
+  var latLng = new google.maps.LatLng(52.959941, 0.588637);
 
   var mapOptions = {
     center: latLng,
     zoom: 14,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    options: {scrollwheel: false}
+    options: { scrollwheel: false },
   };
 
-  var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+  var map = new google.maps.Map(
+    document.getElementById("map_canvas"),
+    mapOptions
+  );
 
   var marker = new google.maps.Marker({
     position: latLng,
     map: map,
-    title:'The Old Maltings'
+    title: "The Old Maltings",
   });
 
-  $('.help-inline').hide();
+  $(".help-inline").hide();
 
-  $('.toggleTrigger').click(function() {
+  $(".toggleTrigger").click(function () {
     //$('.toggleTarget').toggle('slow');
-    if ($('.toggleTarget').is(':hidden')) {
-      $('.toggleTarget').slideDown('slow', 'swing');
-      $(this).html('Hide Terms and Conditions');
-    }
-    else{
-      $('.toggleTarget').hide();
-      $(this).html('Show Terms and Conditions');
+    if ($(".toggleTarget").is(":hidden")) {
+      $(".toggleTarget").slideDown("slow", "swing");
+      $(this).html("Hide Terms and Conditions");
+    } else {
+      $(".toggleTarget").hide();
+      $(this).html("Show Terms and Conditions");
     }
 
     return false;
   });
 
-  $('.nav li').click(function(){
-
-    $(this).addClass('active').siblings().removeClass('active');
-    $('#navigation').collapse('hide');
-
-
+  $(".nav li").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+    $("#navigation").collapse("hide");
   });
 
-  $('.banner').hover(function() {
-    $(this).find('.banner-caption').slideToggle('fast');
+  $(".banner").hover(function () {
+    $(this).find(".banner-caption").slideToggle("fast");
   });
 
   var hasError = false;
-  var showError = function(inputSelector) {
+  var showError = function (inputSelector) {
     $(inputSelector).siblings().first().show();
-    $(inputSelector).closest('.control-group').addClass('error');
+    $(inputSelector).closest(".control-group").addClass("error");
   };
 
-  $('#enquiry').click(function() {
-    var btn = $(this),
-      hasError = false,
-      _name = $('#inputName').val(),
-      _email = $('#inputEmail').val(),
-      _msg = $('#inputMessage').val();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    $('.control-group').removeClass('error');
-    $('.help-inline').hide();
+    var btn = $("#enquiry"),
+      hasError = false,
+      _name = $("#inputName").val(),
+      _email = $("#inputEmail").val(),
+      _msg = $("#inputMessage").val();
+
+    $(".control-group").removeClass("error");
+    $(".help-inline").hide();
 
     if (_name.length === 0) {
-      showError('#inputName');
+      showError("#inputName");
       hasError = true;
     }
     if (_email.length === 0) {
-      showError('#inputEmail');
+      showError("#inputEmail");
       hasError = true;
     }
     if (_msg.length === 0) {
-      showError('#inputMessage');
+      showError("#inputMessage");
       hasError = true;
     }
     if (hasError) {
       return;
     }
-    btn.button('loading');
+    btn.button("loading");
 
-    $.ajax({
-        url: 'https://formspree.io/theoldmaltingsthornham@gmail.com',
-      method: 'POST',
-      data: {message: _msg, email: _email, name: _name},
-      dataType: 'json',
-      success: function (data) {
-        btn.siblings().first().show();
-        btn.closest('.control-group').addClass('success');
-        btn.button('reset');
-      },
-      error: function () {
-        btn.siblings().first().html('Sorry there was an error submitting the form. Please email theoldmaltingsthornham@gmail.com.');
-        btn.siblings().first().show();
-        btn.closest('.control-group').addClass('error');
-        btn.button('reset');
-      }
-    });
-  });
+    const myForm = event.target;
+    const formData = new FormData(myForm);
 
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then((res) => {
+        btn.siblings().first().show();
+        btn.closest(".control-group").addClass("success");
+        btn.button("reset");
+      })
+      .catch((error) => {
+        console.log(error);
+        btn
+          .siblings()
+          .first()
+          .html(
+            "Sorry there was an error submitting the form. Please email theoldmaltingsthornham@gmail.com."
+          );
+        btn.siblings().first().show();
+        btn.closest(".control-group").addClass("error");
+        btn.button("reset");
+      });
+  };
+
+  document.querySelector("form").addEventListener("submit", handleSubmit);
 });
